4dbf9a9fc68350e360f54f22b93c5ec032070189
Timothy Zheng <timothy.zheng@uwaterloo.ca>
2024-12-09 19:32:01 +0800

    day 5 part 1

---
+++ day5/README.txt
@@ -0,0 +1,5 @@
+Formatting for input: 
+1. Add number of rules as first line
+2. replace `|` with ` `
+3. replace `,` with ` `
+4. remove any empty line
---
+++ day5/part2.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int N; cin >> N;
+    int X, Y;
+    unordered_map<int, unordered_set<int>> rules;
+    for (int i = 0; i < N; ++i) {
+        cin >> X >> Y;
+        rules[X].emplace(Y);
+    }
+    // cout << X << " " << Y << endl;
+
+    string line;
+    getline(cin, line); // clear next line
+
+    long long n, ans=0;
+    vector<vector<int>> problematic;
+    while (getline(cin, line)) {
+        // cout << line << endl;
+        vector<int> update;
+        istringstream iss(line);
+        while (iss >> n) {
+            // cout << n << endl;
+            update.emplace_back(n);
+        }
+
+        // check from left to right
+        // check if left update[i] does not conflict with any 
+        bool conflict = false;
+        for (int i = 0; i < update.size() - 1; ++i) {
+            if (conflict) break;
+            for (int j = i+1; j < update.size(); ++j) {
+                if (conflict) break;
+
+                if (rules[update[j]].find(update[i]) != rules[update[j]].end()) conflict = true;
+            }
+        }
+        if (conflict) problematic.emplace_back(update);
+    }
+
+    // todo fix problematic
+
+    for (auto& p: problematic) {
+        // ... do stuff.
+    }
+    cout << ans << endl;
+    return 0;
+}
